4aa97d9401d2e1c2fa62a7ededf46216ca6c65d0
Md Tusriful Rafi <ac119820@avcol.school.nz>
2022-03-18 14:15:05 +1300

    Maori Quiz

--- AS91883 project/Program.cs
+++ AS91883 project/Program.cs
@@ -1,6 +1,6 @@
 ﻿//Introduction\\
 string start;
-string selections, selectionResult;
+int selections, selectionResult;
 string Easy, Medium, Hard;
 Console.WriteLine("Hi There, \n This is a Te Reo Maori (Maori Language) quiz.This is a multichoise quiz but before starting with the quiz you have read the rules!!");
 Console.WriteLine("ELIGIBILITY FOR PARTICIPATING IN THIS QUIZ: The Quiz is open to people (the Participants) anywhere in the world who have completed the age of 10 years. \n BINDING AGREEMENT: In order to enter the Quiz, entrant must accept the official rules.\n The Rules Consist Of: \n (i) You shall not copy the answers \n (ii) you must be over 10 years old. \n (iii) The Participant should read this agreement carefully prior to entry to ensure that he/she understands and agrees, because these Rules form a legally binding agreement with respect to this Quiz.\n (iv) The Quiz will consist of 10 questions for the participants to answer for each level. \n (v) A participant must agree with this terms for the Quiz and complete the Quiz within 5 minutes.");
@@ -8,16 +8,34 @@ Console.WriteLine("ELIGIBILITY FOR PARTICIPATING IN THIS QUIZ: The Quiz is open 
 //QUIZ Level selection\\
 
 start = Console.ReadLine();
-while (start.ToLower() != "start")
+while (start.ToLower() != "s")
 {
-    Console.WriteLine("Please type 'START' to began the QUIZ: ");
+    Console.WriteLine("Please Type 'S' To Start The QUIZ: ");
     start = Console.ReadLine();
 }
-Console.WriteLine("Please type the level you would like to do just typing 'Easy, Medium or Hard' ");
-selections = Convert.ToString(Console.ReadLine());
-selectionResult = selections.ToLower();
-while (selections.ToLower() == 'Easy')
+Console.WriteLine("\n1. Easy");
+Console.WriteLine("2. Medium");
+Console.WriteLine("3. Hard");
+Console.WriteLine("Please type the level you would like to do just typing '1, 2 or 3' ");
+while (!int.TryParse(Console.ReadLine(), out selectionResult) || selectionResult <= 0 || selectionResult >= 4)
 {
-    Console.WriteLine("You have chose quiz level EASY");
-    selections = Console.ReadLine();
+    Console.Write("\nPlease enter a valid level only: ");
 }
+switch (selectionResult)
+{
+    case 1:
+        Console.WriteLine("You have slected Level Easy");
+        Console.WriteLine("This Level will have 10 Basic questions about Te Reo Maori.");
+        break;
+
+    case 2:
+        Console.WriteLine("You have slected Level Medium");
+        break;
+
+    case 3:
+        Console.WriteLine("You have slected Level Hard");
+        break;
+}
+//Level Easy\\
+
+
