882445991170924dbc4bdad2313532b1f5a5ca72
Md Tusriful Rafi <ac119820@avcol.school.nz>
2022-03-24 15:14:35 +1300

    Leve easy

--- AS91883 project/Program.cs
+++ AS91883 project/Program.cs
@@ -1,7 +1,8 @@
 ﻿//Introduction\\
-string start;
-int selections, selectionResult;
+string start,restart;
+int selectionResult;
 string Easy, Medium, Hard;
+
 Console.WriteLine("Hi There, \n This is a Te Reo Maori (Maori Language) quiz.This is a multichoise quiz but before starting with the quiz you have read the rules!!");
 Console.WriteLine("ELIGIBILITY FOR PARTICIPATING IN THIS QUIZ: The Quiz is open to people (the Participants) anywhere in the world who have completed the age of 10 years. \n BINDING AGREEMENT: In order to enter the Quiz, entrant must accept the official rules.\n The Rules Consist Of: \n (i) You shall not copy the answers \n (ii) you must be over 10 years old. \n (iii) The Participant should read this agreement carefully prior to entry to ensure that he/she understands and agrees, because these Rules form a legally binding agreement with respect to this Quiz.\n (iv) The Quiz will consist of 10 questions for the participants to answer for each level. \n (v) A participant must agree with this terms for the Quiz and complete the Quiz within 5 minutes.");
 
@@ -21,21 +22,21 @@ while (!int.TryParse(Console.ReadLine(), out selectionResult) || selectionResult
 {
     Console.Write("\nPlease enter a valid level only: ");
 }
-switch (selectionResult)
-{
-    case 1:
-        Console.WriteLine("You have slected Level Easy");
-        Console.WriteLine("This Level will have 10 Basic questions about Te Reo Maori.");
-        break;
-
-    case 2:
-        Console.WriteLine("You have slected Level Medium");
-        break;
-
-    case 3:
-        Console.WriteLine("You have slected Level Hard");
-        break;
-}
+int LevelEasy, score = 0;
 //Level Easy\\
-
+string answer;
+if (selectionResult == 1)
+{
+    Console.WriteLine("Question 1 \n How to say 'Hello' in Māori");
+    Console.WriteLine("a) Ka Kite \n b) Tino Pai \n c) Tumeke \n d) Kia Ora ");
+   answer = Console.ReadLine();
+    if (answer == "D" || answer == "d")
+    {
+        Console.WriteLine("Correct");
+    }
+    else
+    {
+        Console.WriteLine("Incorrect");
+    }
+}
 
